baf3bcc5dea163a68bd900739618ea353b38db4d
Yves Kaufmann <fxdapokalypse@googlemail.com>
2024-06-04 03:13:26 +0200

    initial commit

---
+++ src/azure-oauth-bearer-provider.spec.ts
@@ -0,0 +1,70 @@
+import { AzureOAuthBearerProvider } from './azure-oauth-bearer-provider';
+
+import { replaceProperty } from 'jest-mock';
+
+const getTokenMock = jest.fn().mockResolvedValue({ token: 'dummy-token', expiresOnTimestamp: 1234567890 });
+jest.mock('@azure/identity', () => ({
+  DefaultAzureCredential: jest.fn().mockImplementation(() => ({
+    getToken: getTokenMock,
+  })),
+}));
+
+describe('AzureOAuthBearerProvider', () => {
+  beforeEach(() => {
+    process.env.EVENTHUB_NAMESPACE = '';
+  });
+
+  describe('brokers', () => {
+    it('should provide the list of brokers (read ns from options)', () => {
+      const azureOAuthProvider = new AzureOAuthBearerProvider({ namespace: 'my-namespace' });
+      expect(azureOAuthProvider.brokers).toEqual(['my-namespace.servicebus.windows.net:9093']);
+    });
+
+    it('should provide the list of brokers (read ns from env.EVENTHUB_NAMESPACE)', () => {
+      replaceProperty(process.env, 'EVENTHUB_NAMESPACE', 'my-namespace');
+
+      const azureOAuthProvider = new AzureOAuthBearerProvider();
+      expect(azureOAuthProvider.brokers).toEqual(['my-namespace.servicebus.windows.net:9093']);
+    });
+
+    it('should throw an error if namespace is not provided', () => {
+      expect(() => {
+        const azureOAuthProvider = new AzureOAuthBearerProvider();
+        azureOAuthProvider.brokers;
+      }).toThrow(/AzureOAuthBearerProvider: namespace is required - .+/);
+    });
+  });
+
+  describe('saslOptions', () => {
+    it('should provide the SASL options to authenticate again eventhub', async () => {
+      const azureOAuthProvider = new AzureOAuthBearerProvider({ namespace: 'my-namespace' });
+      const spyGetBearerToken = jest
+        .spyOn(azureOAuthProvider, 'getBearerToken')
+        .mockResolvedValue({ value: 'my-token' });
+
+      const saslOptions = azureOAuthProvider.saslOptions;
+      expect(saslOptions).toEqual({
+        mechanism: 'oauthbearer',
+        oauthBearerProvider: expect.any(Function),
+      });
+
+      const token = await saslOptions.oauthBearerProvider();
+      expect(token).toEqual({ value: 'my-token' });
+
+      expect(spyGetBearerToken).toHaveBeenCalled();
+    });
+  });
+
+  describe('getBearerToken', () => {
+    it('should request an OAuth bearer token from Azure by scoping the eventhub namesapce', async () => {
+      const azureOAuthProvider = new AzureOAuthBearerProvider({
+        namespace: 'my-namespace',
+      });
+
+      const token = await azureOAuthProvider.getBearerToken();
+      expect(token).toEqual({ value: 'dummy-token' });
+
+      expect(getTokenMock).toHaveBeenCalledWith('https://my-namespace.servicebus.windows.net');
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './azure-oauth-bearer-provider';
